8d8b8484461742e4d53eaeb89a9f8ce6edc3f556
cheng_he <cheng_he@bestsign.cn>
2018-10-30 10:50:14 +0800

    resource server add jwt

--- oauth-resource-server/src/main/java/com/examplecom/hecheng/auth/config/SecurityResourceServerConfig.java
+++ oauth-resource-server/src/main/java/com/examplecom/hecheng/auth/config/SecurityResourceServerConfig.java
@@ -4,13 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
+import org.springframework.security.jwt.crypto.sign.SignatureVerifier;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableResourceServer
@@ -36,8 +47,23 @@ public class SecurityResourceServerConfig extends ResourceServerConfigurerAdapte
         resources.resourceId(RESOURCE_ID).tokenStore(tokenStore());
     }
 
+//    JDBC方式
+//    @Bean
+//    public JdbcTokenStore tokenStore() {
+//        return new JdbcTokenStore(dataSource);
+//    }
+
+    //jwt方式
     @Bean
-    public JdbcTokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
+    public TokenStore tokenStore() {
+        TokenStore tokenStore = new JwtTokenStore(jwtAccessTokenConverter());
+        return tokenStore;
+    }
+
+    private JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+//        jwtAccessTokenConverter.setSigningKey("123");   //测试用
+        jwtAccessTokenConverter.setVerifier(new MacSigner("123"));
+        return jwtAccessTokenConverter;
     }
 }
