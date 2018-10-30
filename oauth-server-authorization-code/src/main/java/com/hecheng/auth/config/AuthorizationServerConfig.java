a0050e6f098e5bc2dc988b4dc9ef9d147c2cd222
cheng_he <cheng_he@bestsign.cn>
2018-10-30 10:22:15 +0800

    认证服务器添加jwt

--- oauth-server-authorization-code/src/main/java/com/hecheng/auth/config/AuthorizationServerConfig.java
+++ oauth-server-authorization-code/src/main/java/com/hecheng/auth/config/AuthorizationServerConfig.java
@@ -1,17 +1,28 @@
 package com.hecheng.auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 认证服务器配置
@@ -48,13 +59,38 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         //*************** 使用JDBC方式 BEGIN*********************//
         clients.withClientDetails(new JdbcClientDetailsService(dataSource));
         //*************** 使用JDBC方式 END *********************//
-
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         //允许使用GET/POST方式请求TOKEN
         endpoints.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
-        endpoints.tokenStore(new JdbcTokenStore(dataSource));
+//        endpoints.tokenStore(new JdbcTokenStore(dataSource));
+        endpoints.tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        TokenStore tokenStore = new JwtTokenStore(jwtAccessTokenConverter());
+        return tokenStore;
+    }
+
+    private JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter() {
+            @Override
+            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+                String userName = authentication.getUserAuthentication().getName();
+                User user = (User) authentication.getUserAuthentication().getPrincipal();
+                /** 自定义一些token属性 ***/
+                final Map<String, Object> additionalInformation = new HashMap<>();
+                additionalInformation.put("userName", userName);
+                additionalInformation.put("roles", user.getAuthorities());
+                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+                OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
+                return enhancedToken;
+            }
+        };
+        jwtAccessTokenConverter.setSigningKey("123");   //测试用
+        return jwtAccessTokenConverter;
     }
 }
