5992a795eed9394b43b070977b386eea4da19d93
cheng_he <cheng_he@bestsign.cn>
2018-11-01 15:18:15 +0800

    rname

--- oauth-authorization-server/src/main/java/com/hecheng/auth/OauthAuthorizationServerApplication.java
+++ oauth-authorization-server/src/main/java/com/hecheng/auth/OauthAuthorizationServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OauthServerAuthorizationCodeApplication {
+public class OauthAuthorizationServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OauthServerAuthorizationCodeApplication.class, args);
+		SpringApplication.run(OauthAuthorizationServerApplication.class, args);
 	}
 }
